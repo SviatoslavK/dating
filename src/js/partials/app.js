c38ef6bfe662d0caafa889c6ec31d3d65a63c8c8
Slava <kolodaslav@gmail.com>
2017-09-08 21:45:31 +0300

    clone project

---
+++ src/js/partials/app.js
@@ -0,0 +1,133 @@
+// Functions for range input
+function getVals() {
+    var parent = this.parentNode;
+    var slides = parent.getElementsByTagName("input");
+    var slide1 = parseFloat(slides[0].value);
+    var slide2 = parseFloat(slides[1].value);
+
+    var myRange = slides[0];
+    var myValue = document.querySelector('.first-range');
+    var off = myRange.clientWidth / (parseInt(myRange.max) - parseInt(myRange.min));
+    var px = ((myRange.valueAsNumber - parseInt(myRange.min)) * off) - (myValue.clientWidth / 2);
+    myValue.style.left = px + 20 + 'px';
+    myValue.innerHTML = myRange.value;
+
+    var myRange2 = slides[1];
+    var myValue2 = document.querySelector('.second-range');
+    var off2 = myRange2.clientWidth / (parseInt(myRange2.max) - parseInt(myRange2.min));
+    var px2 = ((myRange2.valueAsNumber - parseInt(myRange2.min)) * off2) - (myValue2.clientWidth / 2);
+    myValue2.style.left = px2 + 20 + 'px';
+    myValue2.innerHTML = myRange2.value;
+
+    if (slide1 > slide2) { var tmp = slide2; slide2 = slide1; slide1 = tmp; }
+}
+
+// Paginations
+function customPaginate(items) {
+    var paginateContainer = document.querySelector('.gallery__block');
+    var itemsToPaginate = paginateContainer.querySelectorAll('.gallery__card');
+    var pageNumber = Math.ceil((itemsToPaginate.length) / items);
+    var pageNumbersContainer = document.querySelector('.pagination__block');
+
+    for (var i = 0; i < pageNumber; i++) {
+        var page = document.createElement('div');
+        page.classList.add('pagination__page');
+        page.innerHTML = i + 1;
+        pageNumbersContainer.appendChild(page);
+    }
+
+    Array.from(itemsToPaginate).filter(function (item, index) {
+        return index > (items - 1)
+    })
+        .map(function (item) {
+            item.style.display = 'none';
+        })
+    var paginationPages = Array.from(pageNumbersContainer.querySelectorAll('.pagination__page'))
+    paginationPages.map(function (item) {
+        item.addEventListener('click', function () {
+            var number = parseInt(item.textContent);
+            item.classList.add('active__page');
+            var test = paginationPages.filter(function (page) {
+                return page !== item
+            }).map(function (page) {
+                page.classList.remove('active__page')
+            })
+
+            Array.from(itemsToPaginate).map(function (item, index) {
+                if ((index + 1) > ((number - 1) * items) && (index) < (number * items)) {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            })
+        })
+    })
+}
+
+function clearInput(e) {
+    var input = e.target.previousElementSibling
+    input.value = ''
+}
+
+var toTopBtn = document.querySelector('#btn__to-top');
+toTopBtn.addEventListener('click', function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+
+// Change status at card
+function setFavorite() {
+    var allCards = Array.from(document.querySelectorAll('.gallery__card'));
+    allCards.map(function (card) {
+        var favoriteText = card.querySelector('.button__favorite--text');
+        var favoriteBlock = card.querySelector('.card__button--favorite')
+        if (card.hasAttribute('favorite')) {
+            favoriteText.innerHTML = 'В избранных'
+        } else {
+            favoriteText.innerHTML = 'Избранное'
+        }
+
+        favoriteBlock.addEventListener('click', function () {
+            if (card.hasAttribute('favorite')) {
+                card.removeAttribute('favorite')
+                favoriteText.innerHTML = 'Избранное'
+            } else {
+                card.setAttribute('favorite', 'true')
+                favoriteText.innerHTML = 'В избранных'
+            }
+        })
+    })
+}
+
+var allSection = Array.from(document.querySelectorAll('.choose__item'));
+allSection.map(function (item) {
+    item.addEventListener('click', function () {
+        if (item.hasAttribute('checking')) {
+            item.removeAttribute('checking')
+        } else {
+            item.setAttribute('checking', 'true');
+            var rrr = allSection.filter(function(el) {
+                return el !== item
+            }).map(function(el) {
+                el.removeAttribute('checking')
+            })
+        }
+    })
+})
+
+window.onload = setFavorite()
+window.onload = customPaginate(8)
+window.onload = function () {
+    var sliderSections = document.getElementsByClassName("range-slider");
+    for (var x = 0; x < sliderSections.length; x++) {
+        var sliders = sliderSections[x].getElementsByTagName("input");
+        for (var y = 0; y < sliders.length; y++) {
+            if (sliders[y].type === "range") {
+                sliders[y].oninput = getVals;
+                sliders[y].oninput();
+            }
+        }
+    }
+}
+
